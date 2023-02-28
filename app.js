e536cf25b6f2328b9be0a24ed3a51be1fd6873fe
Mohamed Salat <89856630+JustGetAClass@users.noreply.github.com>
2023-02-28 13:04:22 +0300

    ejs setup

--- app.js
+++ app.js
@@ -4,10 +4,24 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'ejs');
+
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-	res.send("Yo Wassup");
+	var today = new Date();
+	var dayOfTheWeek = new Date().toLocaleString("en-us", { weekday: "long" });
+
+	if (today.getDay() === 6 || today.getDay() === 0) {
+		res.write(`<h1>Yaaaay its a ${dayOfTheWeek}</h1>`);
+		res.write("Party time");
+	} else {
+		// res.write(`<h1>Booooo its a ${dayOfTheWeek}</h1>`);
+		// res.write("worky time");
+        res.sendFile(`${__dirname}/index.html`);
+	}
+
+	// res.send();
 });
 
 app.listen(port, () => {
