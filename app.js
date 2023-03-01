4d82ede7f4fc4f1c5893a80bddd005c340bc5dd5
Mohamed Salat <89856630+JustGetAClass@users.noreply.github.com>
2023-03-01 12:12:04 +0300

    new todos show up as list items

--- app.js
+++ app.js
@@ -4,17 +4,28 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 
+var todos = ["Buy Food", "Cook Food", "Eat Food"];
+
 app.get("/", (req, res) => {
 	var date = new Date();
-	var dayIndex = date.getDay();
-	var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-	var day = daysOfWeek[dayIndex];
-	res.render("list", {kindOfDay: day});
+	var options = {
+		weekday: "long",
+		day: "numeric",
+		month: "long",
+	};
 
+	var day = date.toLocaleDateString("en-US", options);
+	res.render("list", {kindOfDay: day, newtodo: todos})
+});
+
+app.post("/", (req, res) => {
+	todo = req.body.todo;
+	todos.push(todo)
+	res.redirect("/")
 });
 
 app.listen(port, () => {
