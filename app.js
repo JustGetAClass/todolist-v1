a7287ed81dfd469401e411b3d81b6ebb3b0283aa
Mohamed Salat <89856630+JustGetAClass@users.noreply.github.com>
2023-03-01 13:38:10 +0300

    added ejs header, footer

--- app.js
+++ app.js
@@ -7,9 +7,10 @@ const port = 3000;
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 let todos = ["Buy Food", "Cook Food", "Eat Food"];
+let worktodos = [];
 
 app.get("/", (req, res) => {
 	let date = new Date();
@@ -20,13 +21,26 @@ app.get("/", (req, res) => {
 	};
 
 	let day = date.toLocaleDateString("en-US", options);
-	res.render("list", {kindOfDay: day, newtodo: todos})
+	res.render("list", { listTitle: day, newtodo: todos });
 });
 
 app.post("/", (req, res) => {
-	todo = req.body.todo;
-	todos.push(todo)
-	res.redirect("/")
+	let todo = req.body.todo;
+	if (req.body.list === "Work List") {
+		worktodos.push(todo);
+		res.redirect("/work");
+	} else {
+		todos.push(todo);
+		res.redirect("/");
+	}
+});
+
+app.get("/work", (req, res) => {
+	res.render("list", { listTitle: "Work List", newtodo: worktodos });
+});
+
+app.get("/about", (req, res) => {
+	res.render("about");
 });
 
 app.listen(port, () => {
