9663a3ac576c08236356261388e01bd0f9dbf84d
Mohamed Salat <89856630+JustGetAClass@users.noreply.github.com>
2023-03-01 12:55:30 +0300

    added css

--- app.js
+++ app.js
@@ -7,18 +7,19 @@ const port = 3000;
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"))
 
-var todos = ["Buy Food", "Cook Food", "Eat Food"];
+let todos = ["Buy Food", "Cook Food", "Eat Food"];
 
 app.get("/", (req, res) => {
-	var date = new Date();
-	var options = {
+	let date = new Date();
+	let options = {
 		weekday: "long",
 		day: "numeric",
 		month: "long",
 	};
 
-	var day = date.toLocaleDateString("en-US", options);
+	let day = date.toLocaleDateString("en-US", options);
 	res.render("list", {kindOfDay: day, newtodo: todos})
 });
 
